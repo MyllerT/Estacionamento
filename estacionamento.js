e41f5df731b0cd02943101cf0119fd21d344266d
Seu Nome <seuemail@example.com>
2023-07-12 16:25:32 -0300

    programaCompleto

---
+++ estacionamento.js
@@ -0,0 +1,72 @@
+import { Bilhete } from "./bilhete.js"
+
+export class Estacionamento {
+    constructor(nome, tbodyControle, preBilhete) {
+        this.nome = nome
+        this._controle = [] //vetor de bilhetes. controle: Bilhete[]
+        this.tbodyControle = tbodyControle
+        this.preBilhete = preBilhete
+        this.bilheteSelecionado = null
+    }
+
+    adicionarBilhete(bilhete) {
+        //TODO adicionar lógica de validação
+        if (!bilhete) throw new InvalidArgumentValue('Bilhete não pode ser nulo')
+        if (!(bilhete instanceof Bilhete)) throw new InvalidArgumentType('Objeto deve ser um bilhete')
+        this._controle.push(bilhete)
+        this._adicionarTBodyRow(bilhete)
+    }
+
+    buscarBilhetePorPlaca(placa) {
+        console.log(`buscando bilhete com a placa ${placa}`)
+        return this._controle.find(bilhete => bilhete.placa === placa)
+    }
+
+    _adicionarTBodyRow(bilhete) {
+        
+        const atualizarPreBilhete = () => {
+            this.preBilhete.innerHTML = `
+            <section>
+                <div>Placa: ${bilhete.placa}</div>
+                <div>Entrada: ${bilhete.entrada}</div>
+            </section>
+            `
+            this.bilheteSelecionado = bilhete
+        }
+
+        const tdOption = document.createElement('td')
+        const tdPlaca = document.createElement('td')
+        const tdEntrada = document.createElement('td')
+
+        const inputRowControle = document.createElement('input')
+        inputRowControle.type = 'radio'
+        inputRowControle.value = bilhete.
+        inputRowControle.id = bilhete.placa
+        inputRowControle.addEventListener('click', atualizarPreBilhete)
+
+        tdOption.appendChild(inputRowControle)
+
+        tdPlaca.textContent = bilhete.placa
+        tdEntrada.textContent = bilhete.entrada
+
+        const trBilhete = document.createElement('tr')
+        trBilhete.appendChild(tdOption)
+        trBilhete.appendChild(tdPlaca)
+        trBilhete.appendChild(tdEntrada)
+
+        this.tbodyControle.appendChild(trBilhete)
+    }
+
+    atualizarTbodyControleResultadoFiltro(bilhete) {
+        // 1- zerar conteúdo
+        
+        tbodyControle.innerHTML = ''
+      // 2- adicionar uma linha que reflita o bilhete
+         this._adicionarTBodyRow(bilhete)
+
+
+    }
+
+    
+}
+
---
+++ main.js
@@ -0,0 +1,63 @@
+import { Estacionamento } from "./estacionamento.js"
+import { Guarita } from "./guarita.js"
+import {Pagamento} from "./pagamento.js"
+
+console.log(`iniciando programa`)
+
+// Mundo Html - Entrada (conexão) de elementos
+const inputPlacaIniciar = document.querySelector("#inputPlacaIniciar")
+const buttonIniciar = document.querySelector("#buttonIniciar")
+
+buttonIniciar.addEventListener('click', iniciarEstadia)
+
+const inputPlacaFiltro = document.querySelector("#inputPlacaFiltro")
+const buttonFiltrar = document.querySelector("#buttonFiltrar")
+const buttonZerarFiltro = document.querySelector("#buttonZerarFiltro")
+
+buttonFiltrar.addEventListener('click', filtrar)
+buttonZerarFiltro.addEventListener('click', zerarFiltro)
+
+//TODO limitar escopo
+const preBilhete = document.querySelector("#preBilhete")
+const buttonEncerrar = document.querySelector("#buttonEncerrar")
+const tbodyControle = document.querySelector("#tbodyControle")
+
+
+buttonEncerrar.addEventListener('click', encerrarEstadia)
+
+
+// Mundo js
+
+  
+
+const estacionamento = new Estacionamento('Meier1',tbodyControle,preBilhete)
+const guarita = new Guarita('Ala leste', estacionamento)
+
+function iniciarEstadia() {
+    const placa = inputPlacaIniciar.value
+    console.log('iniciando estadia para placa: ', placa)
+    guarita.iniciarEstadia(placa)
+}
+
+function encerrarEstadia() {
+    console.log('encerrando estadia')
+    const placa = estacionamento.bilheteSelecionado.placa
+    if (placa) {
+        preBilhete.textContent = guarita.encerrarEstadia(placa)
+    }
+}
+
+function filtrar() {
+    const placa = inputPlacaFiltro.value
+    //1 - pegar bilhete vinculado à placa
+    const bilhete = estacionamento.buscarBilhetePorPlaca(placa)
+    console.log('resultado da busca: ', bilhete)
+    //2 - atualizar conteúdo do tbody da tabela controle
+    //com o resultad do filtro
+    estacionamento.atualizarTbodyControleResultadoFiltro(bilhete)
+}
+
+function zerarFiltro() {
+    console.log('zerando filtros')
+    // 1 - recarregar o conteúdo do controle na tabela
+        estacionamento.carregarTabelaControle()}
